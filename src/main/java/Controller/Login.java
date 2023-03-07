181577b606343463cd450211aaf341b8119b751c
gitpk-0 <kell108p@protonmail.com>
2023-03-07 10:29:58 -0500

    language control + prevent focus TextField on login

--- src/main/java/Controller/Login.java
+++ src/main/java/Controller/Login.java
@@ -2,23 +2,30 @@ package Controller;
 
 import Database.DBUsers;
 import Utility.ChangeView;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 /**
  * @author Patrick Kell
  */
-public class Login {
+public class Login implements Initializable {
 
     ChangeView viewController = new ChangeView();
 
     public TextField username;
     public TextField password;
     public Label loginError;
+    public AnchorPane anchor;
+
 
     public void onLogin(ActionEvent actionEvent) throws IOException, SQLException {
 
@@ -30,5 +37,10 @@ public class Login {
         }
 
         loginError.setText("Your username/password combination was incorrect.");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Platform.runLater(() -> anchor.requestFocus()); // prevents the TextFields from being in focus on start up
     }
 }
--- src/main/java/main/scheduler/MainProgram.java
+++ src/main/java/main/scheduler/MainProgram.java
@@ -2,18 +2,22 @@ package main.scheduler;
 
 import Utility.JDBC;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class MainProgram extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainProgram.class.getResource("/View/Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1020, 840);
+        Scene scene = new Scene(fxmlLoader.load(), 386, 467);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -34,9 +38,36 @@ public class MainProgram extends Application {
 
         // FruitsQuery.select();
         // FruitsQuery.select(2);
+
+
+        Locale france = new Locale("fr", "FR");
+        Locale spain = new Locale("es", "ES");
+        Locale german = Locale.GERMAN;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a language (es, fr, de): ");
+        String langCode = scanner.nextLine();
+
+        if (langCode.equals("fr")) {
+            Locale.setDefault(france); // set language to french
+            System.out.println("Locale = " + Locale.getDefault());
+            ResourceBundle rb = ResourceBundle.getBundle("Nat", Locale.getDefault());
+            System.out.println(rb.getString("hello") + " " + rb.getString("world"));
+        } else if (langCode.equals("es")) {
+            Locale.setDefault(spain);
+            System.out.println("Locale = " + Locale.getDefault());
+            ResourceBundle rb = ResourceBundle.getBundle("Nat", Locale.getDefault());
+            System.out.println(rb.getString("hello") + " " + rb.getString("world"));
+        } else {
+            Locale.setDefault(Locale.ENGLISH);
+        }
+
+
+
+
         launch();
+
         JDBC.closeConnection();
-        // Locale.setDefault(new Locale("fr")); // set language to french
 
     }
 }
