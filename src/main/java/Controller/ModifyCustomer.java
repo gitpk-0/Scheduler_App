9ec2c13b0abd2ac97b521e5f36435d3172a57e5e
gitpk-0 <kell108p@protonmail.com>
2023-03-08 10:55:05 -0500

    modify appt & customer fxml done

--- src/main/java/Controller/ModifyCustomer.java
+++ src/main/java/Controller/ModifyCustomer.java
@@ -1,7 +1,25 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 /**
  * @author Patrick Kell
  */
 public class ModifyCustomer {
+
+    public TextField custIdTF;
+    public TextField nameTF;
+    public TextField phoneTF;
+    public TextField addressTF;
+    public ComboBox countryCombo;
+    public ComboBox divisionCombo;
+    public TextField postalTF;
+
+    public void onSaveCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onCancel(ActionEvent actionEvent) {
+    }
 }
