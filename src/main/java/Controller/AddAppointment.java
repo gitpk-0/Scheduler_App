2811d31f0e3a0d6957840657671da0379a8fbc47
gitpk-0 <kell108p@protonmail.com>
2023-03-08 10:45:59 -0500

    add appointment & customer fxml done

--- src/main/java/Controller/AddAppointment.java
+++ src/main/java/Controller/AddAppointment.java
@@ -1,7 +1,31 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 /**
  * @author Patrick Kell
  */
 public class AddAppointment {
+
+    public TextField titleTF;
+    public TextField descTF;
+    public TextField locationTF;
+    public DatePicker startDatePick;
+    public DatePicker endDatePick;
+    public ComboBox userIdCombo;
+    public ComboBox custIdCombo;
+    public ComboBox contactCombo;
+    public ComboBox endTimeCombo;
+    public ComboBox startTimeCombo;
+    public TextField typeTF;
+
+
+    public void onCancel(ActionEvent actionEvent) {
+    }
+
+    public void onSaveAppt(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/Controller/AddCustomer.java
+++ src/main/java/Controller/AddCustomer.java
@@ -1,7 +1,25 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 /**
  * @author Patrick Kell
  */
 public class AddCustomer {
+
+    public TextField nameTF;
+    public TextField phoneTF;
+    public TextField addressTF;
+    public ComboBox countryCombo;
+    public ComboBox divisionCombo;
+    public TextField postalTF;
+
+
+    public void onSaveCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onCancel(ActionEvent actionEvent) {
+    }
 }
