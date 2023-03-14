a396fdcc888994ef2f3e931b657b9057b05e3d08
gitpk-0 <kell108p@protonmail.com>
2023-03-14 16:27:24 -0400

    main javadocs done

--- src/main/java/main/scheduler/MainProgram.java
+++ src/main/java/main/scheduler/MainProgram.java
@@ -15,9 +15,16 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 /**
- *
+ * MainProgram class of the application which inherits the Application class which sets the stage for the application.
  */
 public class MainProgram extends Application {
+
+    /**
+     * The entry point method of the JavaFX application where all the graphics code of JavaFX is executed
+     *
+     * @param stage The window in which the application appears in
+     * @throws IOException Signals that an Input/Output exception has occurred
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainProgram.class.getResource("/View/Login.fxml"));
@@ -27,11 +34,18 @@ public class MainProgram extends Application {
         stage.show();
     }
 
+    /**
+     * The Scheduler application's entry point
+     *
+     * @param args An array of command-line arguments for the application
+     * @throws SQLException Signals an SQLException has occurred
+     * @throws IOException  Signals that an Input/Output exception has occurred
+     */
     public static void main(String[] args) throws SQLException, IOException {
         // Locale.setDefault(new Locale("fr", "FR"));
-        JDBC.openConnection();
-        launch();
-        JDBC.closeConnection();
-        LoginTracker.cleanUp();
+        JDBC.openConnection(); // open the connection to the database
+        launch(); // launch the JavaFX application
+        JDBC.closeConnection(); //  close the connection to the database
+        LoginTracker.cleanUp(); // close the login_activity.txt file
     }
 }
